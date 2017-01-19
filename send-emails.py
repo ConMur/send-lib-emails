1993e112b696af6068bb52602512e1398d0fdc2e
Connor Sebastian Fraser Murphy <csfmurph@strombola.csclub.uwaterloo.ca>
2017-01-19 08:20:02 -0500

    Added the start of a function to send emails

--- send-emails.py
+++ send-emails.py
@@ -7,6 +7,9 @@ import argparse
 #Import sys so that we can exit when given bad command line args
 import sys
 
+#Import re for regex tokenizing of the file input
+import re
+
 #Set up the parser
 parser = argparse.ArgumentParser()
 parser.add_argument("days",type=int, default = 21, help="This is the maximum number of days ago that a book can be signed out without this script sending an email")
@@ -20,6 +23,20 @@ if days < 0:
 
 #Open the file that contains the list of signed out library books
 signed_out_books_file = open("checkedout.txt", "r")
+
+for line in f:
+    tokens = re.split(" ", line)
+    send_email(tokens[0], tokens[1], tokens[2:])
+
+def send_email(quest_id: str, date: str, book_name_list) -> None:
+    """Sends an email to quest_id@csclub.uwaterloo.ca if the date the book
+    was signed out exceeds the days it is supposed to be signed out for"""
+
+    email_address = quest_id + "@csclub.uwaterloo.ca"
+    book_name = "".join(book_name_list)
+
+    #Check the date to see if the book has been signed out too long
+    
 
 #TODO: parse contents of file and send emails if they have been checked out for
 # longer than "days" days

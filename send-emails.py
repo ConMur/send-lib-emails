fc41deb5bc3d19c32f667110dfaf864f5d952c5a
Connor Murphy <csfmurph@csclub.uwaterloo.ca>
2017-01-25 12:09:01 -0500

    Fixed all syntax errors; need to figure out how to send the email

--- send-emails.py
+++ send-emails.py
@@ -1,4 +1,4 @@
-#Import stmplib for the email sending function
+#Import smtp for the email sending function
 import smtplib
 
 #Import argparse to parse command line args
@@ -14,6 +14,48 @@ import re
 from datetime import date
 import time
 
+#Import getpass for password input
+import getpass
+
+#Import subprocess for validation of if a user is in a group
+import subprocess
+
+#Set up the parser
+parser = argparse.ArgumentParser()
+parser.add_argument("days",type=int, default = 21, help="This is the maximum number of days ago that a book can be signed out without this script sending an email")
+parser.add_argument("--librarianName", default = "Librarian", help="This is the name that will be displayed in the "+
+                     "signature line of the email")
+args = parser.parse_args()
+days = args.days
+
+#Ensure the days are not less than 0
+if days < 0:
+    print("ERROR: days must be greater than 0")
+    sys.exit
+
+#Get the login info from the user
+print("You will be prompted to enter your csclub login for this script to access the mail server."+
+      " Please note this will only work if you are a member of the exec group")
+csclubID = input("Enter your csclubID: ")
+
+#Validate that the user is in the exec group
+try:
+    output = subprocess.check_output(["groups",  csclubID])
+except subprocess.CalledProcessError as  e:
+    print("ERROR: Invalid input")
+    sys.exit
+
+if not "exec" in str(output):
+    print("ERROR: The user " + csclubID + " is not in the exec group.")
+    sys.exit
+
+csclubPwd = getpass.getpass()
+
+#Set up email
+server = smtplib.SMTP("mail.csclub.uwaterloo.ca", 587)
+server.starttls()
+server.login(csclubID,csclubPwd)
+
 #Function definitons
 def send_email(quest_id: str, date: str, book_name_list) -> None:
     """Sends an email to quest_id@csclub.uwaterloo.ca if the date the book
@@ -23,6 +65,8 @@ def send_email(quest_id: str, date: str, book_name_list) -> None:
     book_name = "".join(book_name_list)
 
     if overdue(days, date):
+        email_message ="Hi " + quest_id + ",\n\n" +  "Our records indicate that you have had the book "+ book_name + "signed out for " + str(days) + " days.\n\n" +  "Please return the book to the CS Club office (MC 3036) at your earliest convenience.\n\n" + "Thanks,\n\n" + args.librarianName + "Librarian\n" + "Computer Science Club | University of Waterloo\n" + "librarian@csclub.uwaterloo.ca"
+ 
         print("sending email for book {}".format(book_name))
     else:
         print("not sending email for book {}".format(book_name))
@@ -40,23 +84,19 @@ def overdue(max_days_can_be_signed_out:int, signed_out_date: str) -> bool:
     #Check the date to see if the book has been signed out too long
     return days_signed_out.days > max_days_can_be_signed_out
 
-#Set up the parser
-parser = argparse.ArgumentParser()
-parser.add_argument("days",type=int, default = 21, help="This is the maximum number of days ago that a book can be signed out without this script sending an email")
-args = parser.parse_args()
-days = args.days
-
-#Ensure the days are not less than 0
-if days < 0:
-    print("Error: days must be greater than 0")
-    sys.exit
 
 #Open the file that contains the list of signed out library books
 signed_out_books_file = open("checkedout.txt", "r")
 
 for line in signed_out_books_file:
     tokens = line.split(" ") 
-    send_email(tokens[0], tokens[1], tokens[2:])
+    
+    #Change book name into a space seperated name
+    book_name_with_spaces = ""
+    for i in range(len(tokens)):
+        book_name_with_spaces += str(tokens[i - 2]) + " "
+
+    send_email(tokens[0], tokens[1], book_name_with_spaces)
     
 
 #TODO: parse contents of file and send emails if they have been checked out for
